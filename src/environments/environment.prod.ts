072d3b4f1b08010404a1c9378ccf0af4aadfe6c6
Farangi <elfarangi@gmail.com>
2018-05-11 06:44:41 +0500

    dist created

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAFJfFEGgT7zphPxKk2BEOm-_vIQVWQk8M',
+    authDomain: 'yello-post-social.firebaseapp.com',
+    databaseURL: 'https://yello-post-social.firebaseio.com',
+    projectId: 'yello-post-social',
+    storageBucket: 'yello-post-social.appspot.com',
+    messagingSenderId: '17721288600'
+  }
 };
