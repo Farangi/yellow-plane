89ebb4c634c4b6b0166f22a47a3bea6a653459d9
azzam <elfarangi@gmail.com>
2018-07-31 16:20:40 +0500

    Cleanup added

--- src/app/shared/components/chat-sidebar/chat-sidebar.component.ts
+++ src/app/shared/components/chat-sidebar/chat-sidebar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatSidebarComponent implements OnInit {
 
-  chats:any = {close:[], others:[]};
+  chats:any = [];
 
   constructor() { }
 
@@ -16,16 +16,16 @@ export class ChatSidebarComponent implements OnInit {
   }
 
   getChats(){
-    this.chats.close.push({img:'/assets/img/avatar67-sm.jpg', name: 'Maryam Ali', status:'online'});
-    this.chats.close.push({img:'/assets/img/avatar62-sm.jpg', name: 'Neelam Muneer', status:'status-invisible'});
-    this.chats.close.push({img:'/assets/img/avatar68-sm.jpg', name: 'Haider Ali', status:'online'});
-    this.chats.close.push({img:'/assets/img/avatar69-sm.jpg', name: 'Neem Akram', status:'away'});
-    this.chats.close.push({img:'/assets/img/avatar70-sm.jpg', name: 'Huma Khan', status:'online'});
-    this.chats.others.push({img:'/assets/img/avatar71-sm.jpg', name: 'Ikhlaq Ahmad', status:'away'});
-    this.chats.others.push({img:'/assets/img/avatar72-sm.jpg', name: 'Zeeshan Khan', status:'status-invisible'});
-    this.chats.others.push({img:'/assets/img/avatar63-sm.jpg', name: 'Mustafa Ali', status:'away'});
-    this.chats.others.push({img:'/assets/img/avatar60-sm.jpg', name: 'Waqar Ahsan', status:'online'});
-    this.chats.others.push({img:'/assets/img/avatar56-sm.jpg', name: 'Umar Kamboh', status:'disconnected'});
+    this.chats.push({img:'/assets/img/avatar67-sm.jpg', name: 'Maryam Ali', status:'online'});
+    this.chats.push({img:'/assets/img/avatar62-sm.jpg', name: 'Neelam Muneer', status:'status-invisible'});
+    this.chats.push({img:'/assets/img/avatar68-sm.jpg', name: 'Haider Ali', status:'online'});
+    this.chats.push({img:'/assets/img/avatar69-sm.jpg', name: 'Neem Akram', status:'away'});
+    this.chats.push({img:'/assets/img/avatar70-sm.jpg', name: 'Huma Khan', status:'online'});
+    this.chats.push({img:'/assets/img/avatar71-sm.jpg', name: 'Ikhlaq Ahmad', status:'away'});
+    this.chats.push({img:'/assets/img/avatar72-sm.jpg', name: 'Zeeshan Khan', status:'status-invisible'});
+    this.chats.push({img:'/assets/img/avatar63-sm.jpg', name: 'Mustafa Ali', status:'away'});
+    this.chats.push({img:'/assets/img/avatar60-sm.jpg', name: 'Waqar Ahsan', status:'online'});
+    this.chats.push({img:'/assets/img/avatar56-sm.jpg', name: 'Umar Kamboh', status:'disconnected'});
   }
 
 }
